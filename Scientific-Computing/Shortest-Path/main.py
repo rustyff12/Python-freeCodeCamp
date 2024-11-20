59adcd251a8981069319aaa47638a0a45dcf3136
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2024-11-20 23:08:30 +0100

    shortest path initial set up

---
+++ Scientific-Computing/Shortest-Path/main.py
@@ -0,0 +1,20 @@
+# Shortest Path Algorithm
+my_graph = {
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
+}
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+    while unvisited:
+        pass
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
+    
+#shortest_path(my_graph, 'A')
+
+# Step 40
