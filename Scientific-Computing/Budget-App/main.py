fd883dc6ac906d58766bae35cf045d8da11448a4
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2024-12-23 00:18:06 +0100

    Budget: Main class methods and __str__ completed

--- Scientific-Computing/Budget-App/main.py
+++ Scientific-Computing/Budget-App/main.py
@@ -5,45 +5,88 @@ class Category:
         self.ledger = []
 
     def deposit(self, amount, description = ""):
-        amount = f"{amount:.2f}"
-        self.ledger.append({'amount': amount, 'description': description})
+        self.ledger.append({"amount": amount, "description": description})
         
 
-    def withdraw(self):
-        pass
+    def withdraw(self, amount, description = ""):
+        if not self.check_funds(amount):
+            return False
+        self.ledger.append({"amount": -amount, "description": description})
+        return True
 
     def get_balance(self):
-        pass
+        balance = 0
+        for item in self.ledger:
+            balance += item["amount"]
+        return balance
 
-    def transfer(self):
-        pass
+    def transfer(self, amount, other_category):
+        # if self.get_balance() < amount:
+        if not self.check_funds(amount):
+            return False
+        
+        self.withdraw(amount, f"Transfer to {other_category.name}")
+        other_category.deposit(amount, f"Transfer from {self.name}")
+        return True
 
-    def check_funds(self):
-        pass
+    def check_funds(self, amount):
+        if amount > self.get_balance():
+            return False
+        return True
 
+    # def define_gap(self, left, right):
+    #     gap_num = 30 - (len(left) + len(right))
+    #     if gap_num <= 0:
+    #         trim_num = 29 - len(right)
+    #         left = left[:trim_num]
+    #         gap = " "
+    #     else:
+    #         gap = " " * gap_num
+    #     return f"{left}{gap}{right}"
+
+    # def __str__(self):
+    #     stars_num = (30 - len(self.name)) // 2
+    #     stars = "*" * stars_num 
+        
+    #     res_str = f"{stars}{self.name}{stars}\n"
+        
+    #     initial_deposit = self.define_gap("initial deposit", f"{self.ledger[0]['amount']:.2f}")
+    #     res_str += initial_deposit +"\n"
+        
+    #     for item in self.ledger[1:]:
+    #         res_str += self.define_gap(item["description"], f"{item['amount']:.2f}") + "\n"
+           
+
+    #     total = self.get_balance()
+    #     res_str += f"Total: {total}"
+    #     return res_str
+    def __str__(self):    
+        title = self.name.center(30, '*')  
+        result = f"{title}\n"
     
+        for item in self.ledger:
+            
+            description = item["description"][:23]
+            amount = f"{item['amount']:.2f}".rjust(7)
+            result += f"{description:<23}{amount}\n"
 
-    def __str__(self):
-        stars_num = (30 - len(self.name)) // 2
-        stars = "*" * stars_num 
-        initial_deposit = define_gap("initial deposit", self.ledger[0]['amount'])
-        # initial deposit\t{self.ledger[0]['amount']}
-        return f"{stars}{self.name}{stars}\n{initial_deposit}"
+        total = self.get_balance()
+        result += f"Total: {total:.2f}"
+        return result
 
-def define_gap(left, right):
-        gap_num = 30 - (len(left) + len(right))
-        gap = " " * gap_num
-        return f"{left}{gap}{right}"
+
 
 def create_spend_chart(cat_lst):
     pass
 
 food = Category('Food')
 food.deposit(1000, 'deposit')
-# food.withdraw(10.15, 'groceries')
-# food.withdraw(15.89, 'restaurant and more food for dessert')
-# clothing = Category('Clothing')
-# food.transfer(50, clothing)
+food.withdraw(10.15, 'groceries')
+food.get_balance()
+food.withdraw(15.89, 'restaurant and more food for dessert')
+clothing = Category('Clothing')
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
 print(food)
 
 # *************Food*************
@@ -52,3 +95,7 @@ print(food)
 # restaurant and more foo -15.89
 # Transfer to Clothing    -50.00
 # Total: 923.96
+# ***********Clothing***********
+# Transfer from Food       50.00
+#                         -25.55
+# Total: 24.55
