6f643b1dad8d9ea665c79101fd3fda7cf65ee8b7
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2024-12-27 23:14:51 +0100

    set up of first two classes

--- Scientific-Computing/Equation-Solver/main.py
+++ Scientific-Computing/Equation-Solver/main.py
@@ -1,2 +1,33 @@
-class Equation:
-    pass
+from abc import ABC, abstractmethod
+class Equation(ABC):
+    degree: int
+    
+    def __init__(self, *args):
+        if (self.degree + 1) != len(args):
+            raise TypeError(
+                f"'{self.__class__.__name__}' object takes {self.degree + 1} positional arguments but {len(args)} were given"
+            )
+    def __init_subclass__(cls):
+        if not hasattr(cls, "degree"):
+            raise AttributeError(
+                f"Cannot create '{cls.__name__}' class: missing required attribute 'degree'"
+            )
+    
+    @abstractmethod
+    def solve(self):
+        pass
+        
+    @abstractmethod
+    def analyze(self):
+        pass
+        
+class LinearEquation(Equation):
+    degree = 1
+    
+    def solve(self):
+        pass
+    
+    def analyze(self):
+        pass
+    
+lin_eq = LinearEquation(2, 3)
